73bff1f0b3ca39d8ad63fbf9e83794ef8367148f
Samuel Laroche <samuellaroche@live.ca>
2021-01-15 23:43:05 -0500

    Created base App

---
+++ src/main/java/com/github/jukeboxConfigManager/JukeboxConfigManagerApp.java
@@ -0,0 +1,11 @@
+package com.github.jukeboxConfigManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JukeboxConfigManagerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(JukeboxConfigManagerApp.class, args);
+    }
+}
